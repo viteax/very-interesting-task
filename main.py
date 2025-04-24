151de964adb19e6adf3ba9aa14c4948866a94c59
Viktor Iptyshev <iptvik@gmail.com>
2025-04-24 15:00:07 +0700

    Added headings feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+from clients.stepik import StepikClient
 from clients.word import WordClient
 from logic import IMGS_PATH, get_code_solutions
 
@@ -9,21 +10,27 @@ TEMPLATE_PATH = "assets/template.docx"
 def main():
     os.makedirs(IMGS_PATH, exist_ok=True)
 
-    doc = WordClient(doc_name=TEMPLATE_PATH)
+    doc = WordClient(doc_path=TEMPLATE_PATH)
+    stepik = StepikClient()
 
-    lesson_id = input("Введите id урока: ")
-    current_no = int(input("Начать с номера: "))
+    course_id = int(input("Введите id курса: "))
+    section_no = int(input("Введите номер секции: "))
+    current_no = int(input("Начать нумерацию с: "))
     doc_name = input("Сохранить как: ")
 
-    code_solutions = get_code_solutions(lesson_id=lesson_id)
-    for solution in code_solutions:
-        doc.add_solution(
-            no=current_no,
-            title=solution.title,
-            descr="\n".join(solution.description),
-            img_path=solution.img_path,
-        )
-        current_no += 1
+    lessons = stepik.get_lessons(course_id, section_no)
+    for heading_no, lesson in enumerate(lessons):
+        doc.add_heading2(lesson.title, heading_no=heading_no)
+        code_solutions = get_code_solutions(lesson_id=lesson.id)
+        for solution in code_solutions:
+            doc.add_solution(
+                no=current_no,
+                title=solution.title,
+                descr="\n".join(solution.description),
+                img_path=solution.img_path,
+            )
+            current_no += 1
+        doc.add_page_break()
 
     doc.save(doc_name=doc_name)
 
--- models/stepik.py
+++ models/stepik.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class Lesson(BaseModel):
     id: int
     steps: list[int]
+    title: str
 
 
 class LessonResponse(BaseModel):
